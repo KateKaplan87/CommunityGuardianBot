b73bb6bf701051c3e575e3c670ca2a360041c9bc
KateKaplan87 <Auronffx1987@gmail.com>
2025-04-29 20:01:19 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
+import discord
+from discord.ext import commands
+import os
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+PREFIX = os.getenv("BOT_PREFIX", "!")
+
+bot = commands.Bot(command_prefix=PREFIX, intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} ist online!")
+
+def run_discord_bot():
+    bot.run(TOKEN)
 # Hier wird der Hauptbot-Code reinkommen
