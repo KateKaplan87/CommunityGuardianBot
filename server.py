a9b58f9fc7d6081f9b1d65b61f4ccb6e2a738b9e
KateKaplan87 <Auronffx1987@gmail.com>
2025-04-29 20:02:08 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def home():
-    return "Bot is alive!", 200
 
-if __name__ == "__main__":
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot läuft!"
+
+def run():
     app.run(host="0.0.0.0", port=8080)
