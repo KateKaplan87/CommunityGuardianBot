56e07391515eb77b03e65271deecc80b797f3d24
KateKaplan87 <Auronffx1987@gmail.com>
2025-04-30 06:55:47 +0200

    Update server.py

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ def home():
     return "CommunityGuardianBot is running!"
 
 def run_bot():
-    bot.run_bot()  # startet den Discord-Bot aus bot.py
+    bot.run_discord_bot()  # startet den Discord-Bot aus bot.py
 
 if __name__ == "__main__":
     threading.Thread(target=run_bot).start()  # startet den Bot parallel
