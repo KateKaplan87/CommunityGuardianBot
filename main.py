6a1357ad3c17915b357130816bc71581ec8b87da
KateKaplan87 <Auronffx1987@gmail.com>
2025-04-30 03:40:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ threading.Thread(target=server.run).start()
 
 # Starte den Discord-Bot
 bot.run_discord_bot()
+
+
+from server import keep_alive
+from bot import run_discord_bot
+
+if __name__ == '__main__':
+    keep_alive()
+    run_discord_bot()
